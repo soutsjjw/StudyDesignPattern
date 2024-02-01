9ae9097b97d4d109bab01383fe3b77ee33f0df4b
souts jjw <souts.jjw@gmail.com>
2024-02-02 00:10:01 +0800

    單例模式 Singleton

---
+++ StudyDesignPattern.Src/singleton/Singleton.cs
@@ -0,0 +1,14 @@
+﻿namespace StudyDesignPattern.Src.singleton;
+
+public class Singleton
+{
+    private static readonly Lazy<Singleton> lazyInstance = new Lazy<Singleton>(() => new Singleton());
+
+    private Singleton() { }
+
+    public static Singleton GetInstance
+    {
+        get { return lazyInstance.Value; }
+    }
+}
+
---
+++ StudyDesignPattern.Test/singleton/SingletonTest.cs
@@ -0,0 +1,49 @@
+﻿using StudyDesignPattern.Src.singleton;
+
+namespace StudyDesignPattern.Test.singleton;
+
+public class SingletonTest
+{
+    [Test]
+    [Category("單例模式")]
+    public void Test()
+    {
+        /*
+        // In a single thread, s1 and s2 are indeed the same object
+        Singleton s1 = Singleton.GetInstance();
+        Singleton s2 = Singleton.GetInstance();
+        Console.WriteLine($"s1: {s1.GetHashCode()} s2: {s2.GetHashCode()}");
+        Console.WriteLine();
+        */
+
+        SingletonThread test1 = new SingletonThread("執行序T1");
+        SingletonThread test2 = new SingletonThread("執行序T2");
+
+        Thread t1 = new Thread(new ThreadStart(test1.Run));
+        Thread t2 = new Thread(new ThreadStart(test2.Run));
+
+        t1.Start();
+        t2.Start();
+
+        Assert.Pass();
+    }
+}
+
+public class SingletonThread
+{
+    private string myId;
+
+    public SingletonThread(string id)
+    {
+        myId = id;
+    }
+
+    public void Run()
+    {
+        Singleton singleton = Singleton.GetInstance;
+        if (singleton != null)
+        {
+            Console.WriteLine($"{myId}產生 Singleton: {singleton.GetHashCode()}");
+        }
+    }
+}
